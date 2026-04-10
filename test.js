92ccdf03a328137a01193638cf0fc760519c3b88
abidfahad <abidfahad@hotmail.com>
2026-04-10 18:55:56 +0400

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ console.log('Running tests...');
 // Simulate test pass
 if (true) {
  console.log(' Tests passed');
- process.exit(0);
+ process.exit(1);
 } else {
  console.error(' Tests failed');
  process.exit(1);
+ 
 }
